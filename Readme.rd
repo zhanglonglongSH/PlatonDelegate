6c9305a3778c1cb96ea1abe2aa3420d2df1a9e87
zhanglonglong <zhanglonglong@odaily.com>
2021-05-21 15:46:39 +0800

    优化

--- Readme.rd
+++ Readme.rd
@@ -1,5 +1,12 @@
-围绕PlatON社区[https://scan.platon.network/node]  写的领取收益和将收益委托到节点脚本
-本人节点Dragon_Node;
+围绕PlatON社区[https://scan.platon.network/node]
+简单的领取收益和将收益委托到节点的一个脚本
+本人节点Dragon_Node;多委托谢谢。
 
-sudo apt-get update && sudo apt-get install python3-venv tmux git -y&& https://github.com/zhanglonglongSH/PlatonDelegate.git&& cd PlatonDelegate && python3 -m venv venv&& source ./venv/bin/activate && pip3 install -r requirements.txt
+sudo apt-get update&& sudo apt-get install gcc && sudo apt-get install python3.6-venv  && git clone  https://github.com/zhanglonglongSH/PlatonDelegate.git&& cd ~/PlatonDelegate && python3 -m venv venv&& source ./venv/bin/activate && pip3 install -r requirements.txt
+然后修改delegate.py里面的配置。
 
+##启动节点
+ tmux new -s delegate -d ~/PlatonDelegate/venv/bin/python3  delegate.py
+
+##查看启动状态
+  tmux attach -t delegate   【查看完成后 ctrl+b 然后按d退出】
--- delegate.py
+++ delegate.py
@@ -14,26 +14,24 @@ from client_sdk_python.eth import PlatON
 from client_sdk_python.ppos import Ppos
 
 # 设置领取的最小委托lat数
-min_withdraw_rewarddeteg_count = 0.5
+min_withdraw_rewarddeteg_count = 10
 # 设置收益节点最小含有的lat数
-min_withdraw_nodereward_count = 2
+min_withdraw_nodereward_count = 60
 # 设置间隔时间
-sleep_time_seconds = 60*60*1
+sleep_time_seconds = 60*60*2
 
-# 是否是线上
+# 是否是线上 True|False
 is_online = True
 
-# 设置委托地址信息表
+# 设置委托地址信息表  【node_id 复制的时候没有0x】 bf_privkey收益钱包私钥  address收益钱包地址
 addr_list=[
     {
-        # 委托收益信息  bf_privkey：钱包私钥  address：钱包地址
         'benifit_addr_privkey':[
-            {'bf_privkey':'92b7294ea5cc7a4de4fada550d1519ef5f5cb009bc2f4e16c5a33987e4607cbbe','address':'lat1nymlzwgnzega44n7krcc6fmg2x2yjfzjkv2e6h'},
-            {'bf_privkey':'12cc9fadfe743e4f387ba9effdd5aed438ec3795d5416a203c04a2c49ba5774d6','address':'lat1x7jqhdwydce64q0kmpafzylevxww8ymdvzez8w'},
-            {'bf_privkey':'bf2fddfb0215581b21eed8b3dfb9f65f22098cb6135277c40e82c46934b46e2e81','address':'lat1g00wa9vph9hm5y4434gpuutyj2dcxkvy4fzlqy'},
+            {'bf_privkey':'9b7294ea5cc7a4de4fada550d1519ef5f5cb009bc2f4e16c5a33987e4607cbbe','address':'lat1nymlzwgnzega44n7krcc6fmg2x2yjfzjkv2e6h'},
+            {'bf_privkey':'1cc9wfadfe743e4f387ba9effdd5aed438ec3795d5416a203c04a2c49ba5774d6','address':'lat1x7jqhdwydce64q0kmpafzylevxww8ymdvzez8w'},
+            {'bf_privkey':'bffddfb0d215581b21eed8b3dfb9f65f22098cb6135277c40e82c46934b46e2e8','address':'lat1g00wa9vph9hm5y4434gpuutyj2dcxkvy4fzlqy'},
         ],
-        # 节点id
-        'node_id':"95cae658a18c6b1334a205cdb76599ca810907d8f33f737fa41cbdaba39997112d7f8f46c56c1abcda8d5129eb1d7d494305802300fdd33f10dc6d92f7118574"
+        'node_id':"95cae658a18c6b1d334a205cdb76599ca810907d8f33f737fa41cbdaba39997112d7f8f46c56c1abcda8d5129eb1d7d494305802300fdd33f10dc6d92f7118574"
     }
 ]
 
@@ -45,7 +43,7 @@ def deal( ppos, platon, addr_list):
         for benifit_addr_privkey_obj in benifit_addr_privkey:
             address = benifit_addr_privkey_obj.get("address")
             bf_privkey = benifit_addr_privkey_obj.get("bf_privkey")
-            if(Web3.isAddress(address) == True):
+            if(Web3.isAddress(address) == False):
                 logging.error("错误地址address：".address)
                 continue;
 
@@ -72,8 +70,9 @@ def deal( ppos, platon, addr_list):
                     logging.error(f'🤷‍♂️ Parse error:节点地址委托失败!!')
 
 # 设置网络
-chain_id = 100 if is_online == True else 210309
-w3 = Web3(HTTPProvider("http://47.241.98.219:6789"),chain_id=chain_id);
+chain_id = 100 if is_online == True else 210309;
+host= '127.0.0.1' if is_online == True else '128.199.119.63';
+w3 = Web3(HTTPProvider("http://{host}:6789".format(host)),chain_id=chain_id);
 ppos = Ppos(w3);
 platon = PlatON(w3);
 
